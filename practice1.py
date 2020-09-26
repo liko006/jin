758ff39bb4ed27f145e6ad30b59ecf310f630af4
liko006 <67811331+liko006@users.noreply.github.com>
2020-09-26 13:42:33 +0900

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -16,3 +16,17 @@ df2 = pdr.DataReader('AAPL','yahoo', start, end)
 df3 = pdr.DataReader('LISP.SW','yahoo', start, end)
 
 print(df1.head())
+
+# 불러와서 저장한 데이터의 칼럼 이름 바꾸기
+
+df1.rename(columns={'High':'kor_High','Low':'kor_Low','Open':'kor_Open',
+                   'Close':'kor_Close','Adj Close':'kor_Adj Close'}, inplace=True)
+df2.rename(columns={'High':'usa_High','Low':'usa_Low','Open':'usa_Open',
+                   'Close':'usa_Close','Adj Close':'usa_Adj Close'}, inplace=True)
+df3.rename(columns={'High':'eur_High','Low':'eur_Low','Open':'eur_Open',
+                   'Close':'eur_Close','Adj Close':'eur_Adj Close'}, inplace=True)
+
+print(df1.tail())
+print(df2.tail())
+print(df3.tail())
+
